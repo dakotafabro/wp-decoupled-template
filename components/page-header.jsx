443e79532a8bec7755aa482250287354d7e3d042
Dakota Fabro <dakota@weareoutright.com>
2024-08-05 13:52:29 -0700

    initial commit

---
+++ components/page-header.jsx
@@ -0,0 +1,9 @@
+import styles from './page-header.module.css';
+
+export default function PageHeader({ title }) {
+	return (
+		<header className={styles.header}>
+			<h1 className={styles.headerTitle}>{title}</h1>
+		</header>
+	);
+}
